b707a4998a9c33157eda6ab6a3317caeb3070e0a
KirkVM <kirk.vandermeulen@wisc.edu>
2021-02-24 10:32:16 -0600

    added fit statistics and functioning parameter side bar

--- pulldown.py
+++ pulldown.py
@@ -11,31 +11,31 @@ from lmfit import Minimizer,Parameters, report_fit
 #    with open(dfpath,'r') as f:
 #        return f.read()
 #
-#def fluor_counts_predict(Kd,bound_fluor,bline_fluor,pconc,sconc):
+#def fluor_counts_predict(Kd,prot_fluor,bline_fluor,pconc,sconc):
 #    bTerm=1 - pconc/sconc - Kd/sconc #treat protein as "ligand"
 #    squareTerm=np.power(bTerm,2) - 4*pconc*sconc
 #    theta=0.5*(-bTerm - np.sqrt(squareTerm))
-#    fluor=bline_fluor + bound_fluor*(1-theta)
+#    fluor=bline_fluor + prot_fluor*(1-theta)
 #    return fluor
 #
 #def pulldown_l2lossfunc(kdatup,counts,pconc,sconcs):#
-#    Kd,bound_fluor,bline_fluor=kdatup
+#    Kd,prot_fluor,bline_fluor=kdatup
 #    pred_counts=[]
 #    for sconc in sconcs:
-#        pred_counts.append(fluor_counts_predict(Kd,bound_fluor,bline_fluor,pconc,sconc))
+#        pred_counts.append(fluor_counts_predict(Kd,prot_fluor,bline_fluor,pconc,sconc))
 #    delta = [pred_counts - y for pred_counts,y in zip(pred_counts,counts)]
 #    return np.dot(delta,delta)
 
-def predict_fluorcounts(Kd,bound_fluor,bline_fluor,pconc,sconc):
+def predict_fluorcounts(Kd,prot_fluor,bline_fluor,pconc,sconc):
     bTerm= - (pconc + sconc + Kd)
     squareTerm=np.power(bTerm,2) - 4*pconc*sconc
     complex= 0.5*(-bTerm - np.sqrt(squareTerm))
     theta=complex/pconc
-    pred_counts=bline_fluor + bound_fluor * (1-theta)
+    pred_counts=bline_fluor + prot_fluor * (1-theta)
     return pred_counts
 
 def lm_pdminimizer(params,concs,fluor_counts,pconc):
-    pred_counts=predict_fluorcounts(params['Kd'],params['bound_fluor'],params['bline_fluor'],pconc,concs)
+    pred_counts=predict_fluorcounts(params['Kd'],params['prot_fluor'],params['bline_fluor'],pconc,concs)
     return fluor_counts-pred_counts
 
 st.sidebar.title("Pull-down Binding Fit")
@@ -51,63 +51,55 @@ col1,col2=st.sidebar.beta_columns([4,1])
 with col1:
     Kd=st.number_input('Kd (mg/mL)',value=2.3,min_value=0.001,max_value=100.)
 with col2:
-    Kd_hold=st.checkbox('Kd Hold?')
+    Kd_hold=st.checkbox('Kd Fixed?')
 col1,col2=st.sidebar.beta_columns([4,1])
 with col1:
-    bound_fluor=st.number_input('protein fluorescence',value=3500.,min_value=2000.,max_value=5000.) 
+    prot_fluor=st.number_input('protein fluorescence',value=3500.,min_value=2000.,max_value=5000.) 
 with col2:
-    protfluor_hold=st.checkbox('Fluor Hold?')
+    protfluor_hold=st.checkbox('Fluor Fixed?')
 col1,col2=st.sidebar.beta_columns([4,1])
 with col1:
     bline_fluor=st.number_input('baseline fluorescence',value=500.,min_value=0.,max_value=1000.) 
 with col2:
-    blinefluor_hold=st.checkbox('Bline Hold?')
+    blinefluor_hold=st.checkbox('Bline Fixed?')
 #Kd=st.sidebar.number_input('Kd (mg/mL)',value=2.3,min_value=0.001,max_value=100.) 
 #Kd_hold=st.sidebar.checkbox('Hold?')
 
 params=Parameters()
-params.add('Kd',value=1.,min=.001,max=100)
-params.add('bound_fluor',value=3000,min=2000,max=5000)
-params.add('bline_fluor',value=500,min=0,max=1000)
-lmpdminner=Minimizer(lm_pdminimizer,params,fcn_args=(datadf.conc.values,datadf.counts.values,0.5e-6))
-result=lmpdminner.minimize()
-fitconcs=np.linspace(0,10,50)
-fitKd=result.params['Kd'].value
-
-#    bound_fluor=st.number_input('protein fluorescence',value=3500.,min_value=2000.,max_value=5000.) 
-#    bline_fluor=st.number_input('baseline fluorescence',value=500.,min_value=0.,max_value=1000.) 
-#    protfluor_hold=st.checkbox('Fluor Hold?')
-#    blinefluor_hold=st.checkbox('Bline Hold?')
-#Kd=st.sidebar.number_input('Kd (mg/mL)',value=2.3,min_value=0.001,max_value=100.) 
-#Kd_hold=st.sidebar.checkbox('Hold?')
-
-params=Parameters()
-params.add('Kd',value=1.,min=.001,max=100)
-params.add('bound_fluor',value=3000,min=2000,max=5000)
-params.add('bline_fluor',value=500,min=0,max=1000)
+params.add('Kd',value=Kd,min=.001,max=100,vary=(not Kd_hold))
+params.add('prot_fluor',value=3000,min=2000,max=5000,vary=(not protfluor_hold))
+params.add('bline_fluor',value=500,min=0,max=1000,vary=(not blinefluor_hold))
 lmpdminner=Minimizer(lm_pdminimizer,params,fcn_args=(datadf.conc.values,datadf.counts.values,0.5e-6))
 result=lmpdminner.minimize()
 fitconcs=np.linspace(0,10,50)
 fitKd=result.params['Kd'].value
 
 fitfluors=predict_fluorcounts(result.params['Kd'].value,
-                              result.params['bound_fluor'].value,
+                              result.params['prot_fluor'].value,
                               result.params['bline_fluor'].value,
                               0.5e-6,fitconcs)
 fitdf=pd.DataFrame({'conc':fitconcs,'fitcounts':fitfluors})
 
-dchart=alt.Chart(datadf).mark_circle(size=100).encode(alt.X('conc',scale=alt.Scale(type='linear')),
-                                                      alt.Y('counts')
+dchart=alt.Chart(datadf).mark_circle(size=100).encode(alt.X('conc',scale=alt.Scale(type='linear'),title='Substrate Concentration (mg/mL)'),
+                                                      alt.Y('counts',title="Fluor Counts")
                                                       )
 fchart=alt.Chart(fitdf).mark_line().encode(alt.X('conc'),alt.Y('fitcounts'))
 xchart=alt.layer(dchart,fchart)
 st.altair_chart(xchart,use_container_width=True)
 #st.text_area(report_fit(result))
-dumpstr=f"Kd={result.params['Kd'].value}\n"
-dumpstr+=f'protein fluor. counts={result.params["bound_fluor"].value}\n'
-dumpstr+=f'bline fluor.={result.params["bline_fluor"].value}\n'
+dumpstr=f"Kd= {result.params['Kd'].value:.2g}"
+dumpstr+=f" \u00B1 {result.params['Kd'].stderr:.2g}\n"
+dumpstr+=f'protein fluor counts= {result.params["prot_fluor"].value:.0f}'
+dumpstr+=f' \u00B1 {result.params["prot_fluor"].stderr:.0f}\n'
+dumpstr+=f'bline fluor= {result.params["bline_fluor"].value:.0f}'
+dumpstr+=f' \u00B1 {result.params["bline_fluor"].stderr:.0f}\n'
 st.text(dumpstr)
 
+fitstr=f"\u03C7\u00B2 = {result.chisqr:.0f}\n"
+#fitstr+=f"{result.redchi:.0f} "
+st.text(fitstr)
+
+#st.text(params.pretty_print())
 
 #combodf=pd.concat([datadf,fitdf])
 #melty=pd.melt(combodf,id_vars=['conc'])
